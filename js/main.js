6b7661940ae1058955cc473fa9783c61fe65bd3e
Martin Maidana <martinmaidana90@gmail.com>
2022-07-05 16:21:03 -0300

    destilo retro y futurista terminado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 let bootcamp = "Bootcamp 592066 - Martin Maidana "
 console.log(bootcamp);
 
+let link = document.querySelector('link')
+
+let retro = () => {
+    link.setAttribute("href", "css/estilos-retro.css")
+}
+
+let futurista = () => {
+    link.setAttribute("href", "css/estilos-futuro.css")
+}
+
+document.querySelector('#retro').addEventListener('click', retro)
+
+document.querySelector('#futuro').addEventListener('click', futurista)
 
 
 
